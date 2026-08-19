98ef37f760b55ef58c6da0d9482975c2dd11f3cb
Eliesdras Dias <a0xlotus@gmail.com>
2026-08-19 07:50:32 -0300

    refactor: cria middleware de cors e limpa handlers

--- backend/estoque/cmd/api/main.go
+++ backend/estoque/cmd/api/main.go
@@ -11,6 +11,23 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// corsMiddleware
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Configurações de CORS
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
 func main() {
 	// Configuração do banco
 	connStr := "host=localhost port=5432 user=root password=rootpassword dbname=sistema_notas sslmode=disable"
@@ -31,8 +48,8 @@ func main() {
 	produtoRepo := repository.NewProdutoPostgres(db)
 	produtoHandler := handler.NewProdutoHandler(produtoRepo)
 	// Rotas
-	http.HandleFunc("/produtos", produtoHandler.Criar)
-	http.HandleFunc("/produtos/listar", produtoHandler.Listar)
+	http.HandleFunc("/produtos", corsMiddleware(produtoHandler.Criar))
+	http.HandleFunc("/produtos/listar", corsMiddleware(produtoHandler.Listar))
 	// Servidor
 	fmt.Println("Servidor rodando na porta 8080")
 	http.ListenAndServe(":8080", nil)
--- backend/estoque/internal/adapters/handler/produto_handler.go
+++ backend/estoque/internal/adapters/handler/produto_handler.go
@@ -19,10 +19,6 @@ func NewProdutoHandler(repo ports.ProdutoRepository) *ProdutoHandler {
 
 // Criar
 func (h *ProdutoHandler) Criar(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
 		return
@@ -57,9 +53,6 @@ func (h *ProdutoHandler) Listar(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Erro interno ao listar os produtos", http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	w.Header().Set("Content-Type", "application/json")
 
